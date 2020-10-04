da6bcf8ee907ac0d7170db2ddb51488683f3ac7f
cyh <576030221@qq.com>
2020-10-04 23:38:13 +0800

    cyh

---
+++ renren-admin/src/main/resources/statics/js/modules/po_article/poarticle.js
@@ -0,0 +1,135 @@
+$(function () {
+    $("#jqGrid").jqGrid({
+        url: baseURL + 'po_article/poarticle/list',
+        datatype: "json",
+        colModel: [			
+			{ label: 'id', name: 'id', index: 'id', width: 50, key: true },
+			{ label: '景区名', name: 'name', index: 'name', width: 80 }, 			
+			{ label: '封面图路径', name: 'coverImg', index: 'cover_img', width: 80 }, 			
+			{ label: '介绍', name: 'introduce', index: 'introduce', width: 80 }, 			
+			{ label: '详情（富文本）', name: 'details', index: 'details', width: 80 }, 			
+			{ label: '景区评价、星级', name: 'evaluate', index: 'evaluate', width: 80 }, 			
+			{ label: '所在区编码（外键 省市联动表code）', name: 'areaId', index: 'area_id', width: 80 }, 			
+			{ label: '创建时间', name: 'createTime', index: 'create_time', width: 80 }, 			
+			{ label: '类型 1、景区 2、美食', name: 'type', index: 'type', width: 80 }, 			
+			{ label: '状态:1.顶置  2.正常  3.封禁 4.待修改', name: 'status', index: 'status', width: 80 }			
+        ],
+		viewrecords: true,
+        height: 385,
+        rowNum: 10,
+		rowList : [10,30,50],
+        rownumbers: true, 
+        rownumWidth: 25, 
+        autowidth:true,
+        multiselect: true,
+        pager: "#jqGridPager",
+        jsonReader : {
+            root: "page.list",
+            page: "page.currPage",
+            total: "page.totalPage",
+            records: "page.totalCount"
+        },
+        prmNames : {
+            page:"page", 
+            rows:"limit", 
+            order: "order"
+        },
+        gridComplete:function(){
+        	//隐藏grid底部滚动条
+        	$("#jqGrid").closest(".ui-jqgrid-bdiv").css({ "overflow-x" : "hidden" }); 
+        }
+    });
+});
+
+var vm = new Vue({
+	el:'#rrapp',
+	data:{
+		showList: true,
+		title: null,
+		poArticle: {}
+	},
+	methods: {
+		query: function () {
+			vm.reload();
+		},
+		add: function(){
+			vm.showList = false;
+			vm.title = "新增";
+			vm.poArticle = {};
+		},
+		update: function (event) {
+			var id = getSelectedRow();
+			if(id == null){
+				return ;
+			}
+			vm.showList = false;
+            vm.title = "修改";
+            
+            vm.getInfo(id)
+		},
+		saveOrUpdate: function (event) {
+		    $('#btnSaveOrUpdate').button('loading').delay(1000).queue(function() {
+                var url = vm.poArticle.id == null ? "po_article/poarticle/save" : "po_article/poarticle/update";
+                $.ajax({
+                    type: "POST",
+                    url: baseURL + url,
+                    contentType: "application/json",
+                    data: JSON.stringify(vm.poArticle),
+                    success: function(r){
+                        if(r.code === 0){
+                             layer.msg("操作成功", {icon: 1});
+                             vm.reload();
+                             $('#btnSaveOrUpdate').button('reset');
+                             $('#btnSaveOrUpdate').dequeue();
+                        }else{
+                            layer.alert(r.msg);
+                            $('#btnSaveOrUpdate').button('reset');
+                            $('#btnSaveOrUpdate').dequeue();
+                        }
+                    }
+                });
+			});
+		},
+		del: function (event) {
+			var ids = getSelectedRows();
+			if(ids == null){
+				return ;
+			}
+			var lock = false;
+            layer.confirm('确定要删除选中的记录？', {
+                btn: ['确定','取消'] //按钮
+            }, function(){
+               if(!lock) {
+                    lock = true;
+		            $.ajax({
+                        type: "POST",
+                        url: baseURL + "po_article/poarticle/delete",
+                        contentType: "application/json",
+                        data: JSON.stringify(ids),
+                        success: function(r){
+                            if(r.code == 0){
+                                layer.msg("操作成功", {icon: 1});
+                                $("#jqGrid").trigger("reloadGrid");
+                            }else{
+                                layer.alert(r.msg);
+                            }
+                        }
+				    });
+			    }
+             }, function(){
+             });
+		},
+		getInfo: function(id){
+			$.get(baseURL + "po_article/poarticle/info/"+id, function(r){
+                vm.poArticle = r.poArticle;
+            });
+		},
+		reload: function (event) {
+			vm.showList = true;
+			var page = $("#jqGrid").jqGrid('getGridParam','page');
+			$("#jqGrid").jqGrid('setGridParam',{ 
+                page:page
+            }).trigger("reloadGrid");
+		}
+	}
+});
